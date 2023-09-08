0db00312b3cec72cd852ec02a06511b6047154bc
Suman <sumanmitra07@outlook.com>
2023-09-08 17:27:17 +0530

    conditional_statements

---
+++ 03-conditional_statements.py
@@ -0,0 +1,56 @@
+# 1. If
+# number = int(input("Enter a number: "))
+
+# >, <, >=, <=, ==
+
+# if cond:
+	# TODO
+
+# if number > 0:
+# 	print("It is greater than 0")
+
+
+
+
+# 2. Else
+
+# if number > 0:
+# 	print("Greater than 0")
+# 	# TODO
+# else:
+# 	print("No")
+
+
+
+
+# if -> elif -> else
+# 3. Else If
+
+# if number > 0:
+# 	print("Greater than 0")
+# 	# TODO
+# elif number == 0:
+# 	print("Is equals to 0")
+# else:
+# 	print("No")
+
+
+
+# Example
+number = int(input("Enter a number: "))
+
+# % -> to find reminder
+
+# 12 % 2 = 0 , 13 % 2 = 1
+
+rem = number % 2
+
+if rem == 0:
+	print("Even")
+else:
+	print("Odd")
+
+
+
+
+# 4. Push to Github
---
+++ solve_calculator.py
@@ -0,0 +1,28 @@
+# Variables and Input
+
+# print("Sum")
+# number1 = int(input("Enter 1st Number: "))
+# number2 = int(input("Enter 2nd Number: "))
+# print(f"The sum of {number1} and {number2} is {number1 + number2}")
+
+# print("Subtraction")
+# number1 = int(input("Enter 1st Number: "))
+# number2 = int(input("Enter 2nd Number: "))
+# print(f"The difference of {number1} and {number2} is {number1 - number2}")
+
+
+
+
+# Conditonal Statement
+
+number1 = int(input("Enter 1st Number: "))
+operator = input("Enter the opeartor (+, -, *, /): ")
+number2 = int(input("Enter 2nd Number: "))
+
+if operator == "+":
+	print(f"The sum of {number1} and {number2} is {number1 + number2}")
+elif operator == "-":
+	print(f"The difference of {number1} and {number2} is {number1 - number2}")
+else:
+	print("To be implemneted") # Homework
+

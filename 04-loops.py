21729445ddc40ccfa842d4f8207a16ee854fdc0d
Suman <sumanmitra07@outlook.com>
2023-09-14 16:29:07 +0530

    for and whle loops

---
+++ 04-loops.py
@@ -0,0 +1,34 @@
+# 1. For Loop
+
+string = "Hello World"
+
+
+for letter in string:
+	if letter == "H":
+		continue # skip
+		# TODO
+	# TODO
+	if letter == "l":
+		break  # do not continue further in the loop
+	print(letter, end= " ")
+
+
+
+
+
+# 2. While Loop
+# number = 5
+# while number <= 10:
+# 	print(number)
+# 	number+=1
+# else:
+# 	print(f"The conditon became false at: {number}")
+
+
+
+# 3. Make Calculator better
+
+
+
+
+# 4. Push to Github
--- solve_calculator.py
+++ solve_calculator.py
@@ -15,14 +15,37 @@
 
 # Conditonal Statement
 
-number1 = int(input("Enter 1st Number: "))
-operator = input("Enter the opeartor (+, -, *, /): ")
-number2 = int(input("Enter 2nd Number: "))
+# number1 = int(input("Enter 1st Number: "))
+# operator = input("Enter the opeartor (+, -, *, /): ")
+# number2 = int(input("Enter 2nd Number: "))
 
-if operator == "+":
-	print(f"The sum of {number1} and {number2} is {number1 + number2}")
-elif operator == "-":
-	print(f"The difference of {number1} and {number2} is {number1 - number2}")
-else:
-	print("To be implemneted") # Homework
+# if operator == "+":
+# 	print(f"The sum of {number1} and {number2} is {number1 + number2}")
+# elif operator == "-":
+# 	print(f"The difference of {number1} and {number2} is {number1 - number2}")
+# else:
+# 	print("To be implemneted") # Homework
 
+
+
+# Loops
+should_continue = True
+while should_continue:
+	number1 = int(input("Enter 1st Number: "))
+	operator = input("Enter the opeartor (+, -, *, /): ")
+	number2 = int(input("Enter 2nd Number: "))
+
+	if operator == "+":
+		print(f"The sum of {number1} and {number2} is {number1 + number2}")
+	elif operator == "-":
+		print(f"The difference of {number1} and {number2} is {number1 - number2}")
+	else:
+		print("To be implemneted")
+
+	option = input("Should I continue? (Y/N): ")
+
+	if option == "Y":
+		continue # go back to cond
+	else:
+		should_continue = False
+	
